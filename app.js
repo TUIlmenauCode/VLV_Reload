a694bc1dfc2723c20ff7d2411742b167a901c8bc
Sebastian Speitel <sebastian.speitel@outlook.de>
2018-10-19 20:33:45 +0200

    did stuff

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
 'use strict';
 
 console.log('Hello world');
+
+
+/**
+ * Test class does stuff
+ * @class*/
+class Test {
+
+    /**
+     * Constructs a new Test object
+     * @param {object} config configuration object
+     */
+    constructor(config = {}) {
+
+    }
+
+}
